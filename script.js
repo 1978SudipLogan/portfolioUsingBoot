516364d8408436830a9d2b1a35186a43ee7a969a
1978SudipLogan <kumarsudip053@gmail.com>
2024-07-02 20:41:36 +0530

    second

---
+++ script.js
@@ -0,0 +1,7 @@
+function welcome() {
+    let name = document.getElementById("name").value
+    text.innerHTML = "Thank You for Contacting us, " + name;
+    let text = document.getElementById("sub");
+    text.style.backgroundColor = "rgb(235, 114, 114)";
+    text.style.color = "black";
+}
